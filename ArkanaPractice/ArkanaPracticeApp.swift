c8412f2928584df19d055a0742d302825c75f90d
sumiyama <80329420+onakama@users.noreply.github.com>
2023-06-21 10:39:31 +0900

    Initial Commit

---
+++ ArkanaPractice/ArkanaPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArkanaPracticeApp.swift
+//  ArkanaPractice
+//
+//  Created by taisei.sumiyama on 2023/06/21.
+//
+
+import SwiftUI
+
+@main
+struct ArkanaPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArkanaPractice/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ArkanaPractice
+//
+//  Created by taisei.sumiyama on 2023/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
